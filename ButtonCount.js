55bf91bfeba8eacb548a81ca8fbeb40a69b691cc
Atlae <71896235+Atlae@users.noreply.github.com>
2023-03-15 10:37:33 +0000

    upload

---
+++ ButtonCount.js
@@ -0,0 +1,7 @@
+// All of the code to make button-count work must be written inside of a class titled ButtonCount inside a file titled ButtonCount.js
+// The element button-count must be defined in the custom elements registry (this should be a single line of code underneath your class)
+// button-count must be an autonomous custom element, meaning it should extend HTMLElement, not any other element
+// All of the markup inside of button-count must be inside of the Shadow DOM (i.e. attached to a shadow root)
+// As long as the above is requirements are met, the exact implementation of how it works is up to you.
+
+class ButtonCount {}
